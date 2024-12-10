5d3a584228acb6c5729c3bcabc748e945aed5c47
divya <divyab@peopleone.co>
2024-12-10 06:08:36 +0530

    msg

---
+++ ca1.js
@@ -0,0 +1,16 @@
+const { log } = require("console");
+
+let firstName = "Divya";
+let companyName="peopleOne";
+let phNo="87563421";
+let isAutomation=true;
+let hasPlaywright;
+
+console.log(typeof firstName);
+console.log(typeof companyName);
+console.log(typeof phNo);
+console.log(typeof isAutomation);
+console.log(typeof hasPlaywright);
+
+
+
---
+++ ca2.js
@@ -0,0 +1,13 @@
+const browserVersion="chrome";
+function getBrowserVersion(){
+   
+    if(browserVersion.startsWith("chrome")){
+        
+        let browserVersion="v 131.0.6718.109";
+        console.log(browserVersion);
+    }
+    console.log(browserVersion);
+    
+    
+}
+getBrowserVersion();
